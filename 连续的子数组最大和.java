e64f02b232b52d23e6192c921c62696a853b7c3b
theripe <theripe666@163.com>
2021-07-14 20:49:07 +0800

    每日一题

---
+++ 连续的子数组最大和.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int[] dp = new int[array.length];
+        int max = array[0];
+        dp[0] = array[0];
+        for (int i = 1; i < array.length; i++) {
+            int newMax = dp[i - 1] + array[i];
+            if (newMax > array[i]) {
+                dp[i] = newMax;
+            } else {
+                dp[i] = array[i];
+            }
+            
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+}

903e8c8a373081c3b200af51043f273d7f4c4ffd
theripe <theripe666@163.com>
2021-08-03 22:37:21 +0800

    力扣每日一题

---
+++ 最短无序连续子数组.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int[] arr = nums.clone();
+        Arrays.sort(arr);
+        int i = 0, j = n - 1;
+        while (i <= j && nums[i] == arr[i]) i++;
+        while (i <= j && nums[j] == arr[j]) j--;
+        return j - i + 1;
+    }
+}
